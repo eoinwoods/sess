18c93303aa0dd9744d80b5a848d9ad4513c397d4
Eoin Woods <github@copse.org.uk>
2014-05-10 07:18:50 +0100

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -3,6 +3,7 @@ export EC2_HOME=${EC2_HOME:-$scriptDir/ec2-api-tools-1.6.13.0}
 export PATH=$EC2_HOME/bin:$PATH
 export AWS_ACCESS_KEY=$(cat AWS-credentials.txt | grep AWS_ACCESS_KEY | cut -f2 -d:)
 export AWS_SECRET_KEY=$(cat AWS-credentials.txt | grep AWS_SECRET_KEY | cut -f2 -d:)
+export AWS_GROUP=default
 export AWS_KEYPAIR_FILE=${AWS_KEYPAIR_FILE:-$scriptDir/primary-key-pair.pem}
 export EC2_URL=https://ec2.eu-west-1.amazonaws.com
 export PATH=$scriptDir:$PATH
